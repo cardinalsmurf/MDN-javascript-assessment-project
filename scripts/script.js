018ee2b42e9b5586c32dd8f42d37035ab3386d77
Robert Buckley <cardinalsmurf@gmail.com>
2016-10-31 16:23:39 -0700

    Add changes per MDN instructions

--- scripts/script.js
+++ scripts/script.js
@@ -16,3 +16,37 @@ Bob saw the whole thing, but he was not surprised — :insertx: weighs 300 pound
 var insertX = ['Willy the Goblin', 'Big Daddy', 'Father Christmas'];
 var insertY = ['the soup kitchen', 'Disneyland', 'the White House'];
 var insertZ = ['spontaneously combusted', 'melted into a puddle on the sidewalk', 'turned into a slug and crawled away'];
+
+randomize.addEventListener('click', result);
+
+function result() {
+  var newStory = storyText;
+  var xItem = randomValueFromArray(insertX);
+  var yItem = randomValueFromArray(insertY);
+  var zItem = randomValueFromArray(insertZ);
+  
+  while (newStory.indexOf(':insert' !== -1)) {
+    newStory = newStory.replace(':insertx:', xItem);
+    newStory = newStory.replace(':inserty:', yItem);
+    newStory = newStory.replace(':insertz:', zItem);
+  }
+  
+  if(customName.value != '') {
+    var name = customName.value;
+    newStory = newStory.replace('Bob', name);
+  }
+
+  if(document.getElementById("uk").checked) {
+    var weight = Math.round(300 * 0.0714285714) + ' stone';
+    var temperature =  Math.round((94 -32) * (5 / 9)) + ' centigrade';
+    
+    while ((newStory.indexOf('94 fahrenheit') !== -1) && (newStory.indexOf('300 pounds') !== -1)) {
+      newStory = newStory.replace('94 fahrenheit', temperature);
+      newStory = newStory.replace('300 pounds', weight);
+    }
+    
+  }
+
+  story.textContent = newStory;
+  story.style.visibility = 'visible';
+}
